0ef4735d67bd61a6746a65018e06210975136075
ewind-us <ewind@mail.ustc.edu.cn>
2015-06-16 13:09:41 +0800

    minor fix

--- kmp.py
+++ kmp.py
@@ -1,24 +1,23 @@
 def build_prefix(pattern):
-    prefix = [0 for i in range(len(pattern))]
-    k = 0
+    prefix, k = [0], 0
     for q in range(1, len(pattern)):
         while k > 0 and pattern[k] != pattern[q]:
             k = prefix[k]
         if pattern[k] == pattern[q]:
             k += 1
-        prefix[q] = k
+        prefix.append(k)
     return prefix
 
 def kmp(pattern, text, prefix):
     q = 0
     for i in range(0, len(text)):
-        while q > 0 and pattern[q] != text[i]:
-            q = prefix[max(q-1, 0)]
+        while pattern[q] != text[i] and q > 0:
+            q = prefix[q-1]
         if pattern[q] == text[i]:
             q += 1
         if q == len(pattern):
             print "match at text[%d]" % (i - len(pattern) + 1)
-            q = prefix[max(q-1, 0)]
+            q = prefix[q-1]
 
 
 p = "abcdabd"
